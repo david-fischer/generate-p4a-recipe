59fe89534536df7007165110f5afb84bcd301a1a
David Fischer <d_fi@gmx.de>
2020-07-06 15:41:34 +0200

    add: setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="generate-p4a-recipe",
+    version="0.1",
+    packages=[""],
+    url="github.com/david-fischer/generate-p4a-recipe",
+    license="MIT",
+    author="david-fischer",
+    author_email="david.fischer.git@posteo.de",
+    description="Generates recipe for python-for-android.",
+)
