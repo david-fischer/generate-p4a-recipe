4ae74c9764684912822629fab7f521766f885b12
David Fischer <d_fi@gmx.de>
2020-07-06 20:20:40 +0200

    fix: setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="generate-p4a-recipe",
     version="0.1",
-    packages=["."],
+    packages=[""],
     url="github.com/david-fischer/generate-p4a-recipe",
     license="MIT",
     author="david-fischer",
@@ -17,6 +17,6 @@ setup(
         "pipdeptree",
         "simple_term_menu",
     ],
-    package_data={"": ["README.md", "recipe_template.stache", "LICENSE"]},
+    package_data={"": ["recipe_template.stache"]},
     include_package_data=True,
 )
